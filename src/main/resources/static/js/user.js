c8ed6aa09ee687978ea6aaac112f78e40a8a6e4d
unknown <jjuwa93@naver.com>
2022-05-05 22:08:09 +0900

    220505_38_15.35_stuided

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,30 @@
+let index = {
+	init: function() {
+		$("btn-save").on("click", ()=>{ //function(){} 대신 ()=>{} 사용하는 이유 this를 바인딩하기 위해
+			this.save();
+		})
+	},
+	
+	save: function() {
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+			email: $("#email").val(),
+		};
+		//회원가입 수행 요청
+		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+		$.ajax({
+			type: "POST",
+			url: "/api/user",
+			data: JSON.stringify(data), //http body 데이터, data는 JS오브젝트, JSON.stringify(data)는 JSON문자열
+			contentType: "application/json; charset=utf-8", //body데이터 타입
+			dataType: "json" //요청을 서버로해서 응답이 왔을 때 JSON으로 왔으면 JS오브젝트로 변환 (기본적으로 문자열로 옴)
+		}).done(function() {
+			alert("회원가입이 완료되었습니다.")
+			location.href = "/blog";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); 
+	}
+}
+
