62e75fa8221ae2f3ead7b29f0a796483a5373d43
Popo-Jin <jjuwa93@naver.com>
2022-05-06 14:56:04 +0900

    220506_38_16:00_ajax not working

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,7 +4,6 @@ let index = {
 			this.save();
 		})
 	},
-	
 	save: function() {
 		let data = {
 			username: $("#username").val(),
@@ -15,12 +14,12 @@ let index = {
 		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
 		$.ajax({
 			type: "POST",
-			url: "/api/user",
+			url: "/blog/api/user",
 			data: JSON.stringify(data), //http body 데이터, data는 JS오브젝트, JSON.stringify(data)는 JSON문자열
 			contentType: "application/json; charset=utf-8", //body데이터 타입
 			dataType: "json" //요청을 서버로해서 응답이 왔을 때 JSON으로 왔으면 JS오브젝트로 변환 (기본적으로 문자열로 옴)
-		}).done(function() {
-			alert("회원가입이 완료되었습니다.")
+		}).done(function(resp) {
+			alert("회원가입이 완료되었습니다.");
 			location.href = "/blog";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
