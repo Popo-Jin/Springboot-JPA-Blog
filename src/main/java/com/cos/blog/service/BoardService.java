6570ebc7f8da2fb30e8490e655fbb612793a1329
Popo-Jin <jjuwa93@naver.com>
2022-05-30 18:51:27 +0900

    220530_18:51_deleteboard_studied

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -28,6 +28,7 @@ public class BoardService {
 		boardRepository.save(board);
 	}
 	
+	@Transactional(readOnly = true)
 	public Page<Board> 글목록(Pageable pageable){
 		return boardRepository.findAll(pageable);
 	}
@@ -37,5 +38,10 @@ public class BoardService {
 				.orElseThrow(()-> {
 					return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
 				});
+	}
+	
+	@Transactional
+	public void 글삭제하기(int id) {
+		 boardRepository.deleteById(id);
 	}
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index = {
 		$("#btn-save").on("click", () => { //function(){} 대신 ()=>{} 사용하는 이유 this를 바인딩하기 위해
 			this.save();
 		});
+		$("#btn-delete").on("click", () => { //function(){} 대신 ()=>{} 사용하는 이유 this를 바인딩하기 위해
+			this.deleteById();
+		});
 	},
 	save: function() {
 		let data = {
@@ -18,6 +21,19 @@ let index = {
 			dataType: "json"
 		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+	deleteById: function() {
+		var id = $("#id").text();
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
